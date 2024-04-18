0b9141675b6647d37b11c5cb85e4b207d0bf7b99
Joel <elsuperjoy@gmail.com>
2024-04-18 15:21:38 -0600

    botones y pruebas

--- app/src/main/java/com/example/vigilancia/activity/FinalActivity.kt
+++ app/src/main/java/com/example/vigilancia/activity/FinalActivity.kt
@@ -1,6 +1,7 @@
 package com.example.vigilancia.activity
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.vigilancia.R
 
@@ -9,5 +10,22 @@ class FinalActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // Aquí estableces el layout para la actividad final. Debes crear un archivo XML para esto.
         setContentView(R.layout.actividad_final)
+
+    val btnTomarFoto: Button = findViewById(R.id.btnTomarFoto)
+    val btnVerFotos: Button = findViewById(R.id.btnVerFotos)
+    val btnTerminarVigilancia: Button = findViewById(R.id.btnTerminarVigilancia)
+
+    btnTomarFoto.setOnClickListener {
+        // Inicia la actividad para tomar una foto
     }
+
+    btnVerFotos.setOnClickListener {
+        // Muestra las fotos que se han tomado
+    }
+
+    btnTerminarVigilancia.setOnClickListener {
+        // Finaliza la vigilancia y posiblemente regresa al usuario al inicio
+        finish()
+    }
+}
 }
